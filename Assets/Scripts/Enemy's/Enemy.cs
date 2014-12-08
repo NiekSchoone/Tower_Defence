a1d2a56b35a51a19bc9a7e1b843b019ab205b662
arnoudHauw <fiets_arnoud@live.nl>
2014-12-08 13:23:06 +0100

    Waypoints
    Dit is het waypoint system

---
+++ Assets/Scripts/Enemy's/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public Transform[] waypoints;
+
+	private float speed = 1;
+	private int currentWaypoint;
+
+
+	// Use this for initialization
+	void Start () {
+		currentWaypoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//target = waypoints[currentWaypoint].position;
+		float step = speed * Time.deltaTime;
+		if(transform.position == waypoints[currentWaypoint].transform.position)
+		{
+			currentWaypoint++;
+		}
+		else
+		{
+			transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].position, step);
+		}
+	}
+}
