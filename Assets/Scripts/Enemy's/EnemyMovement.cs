3b51def6bd95ad43642c23adc9921159adb1c5b0
Niek Schoone <16789@ma-web.nl>
2015-01-12 09:45:04 +0100

    commit

--- Assets/Scripts/Enemy's/EnemyMovement.cs
+++ Assets/Scripts/Enemy's/EnemyMovement.cs
@@ -29,17 +29,12 @@ public class EnemyMovement : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-<<<<<<< HEAD
-	void Update () {
-		Destroy ();
-=======
 	void Update () 
 	{
 		if(_currentWaypoint >= (waypoints.Count))
 		{
 			Destroy ();
 		}
->>>>>>> origin/master
 
 		//speed for Enemey.
 		float step = _speed * Time.deltaTime;
@@ -52,16 +47,9 @@ public class EnemyMovement : MonoBehaviour {
 		//transform.LookAt(waypoints[_currentWaypoint].transform.position);
 	}
 
-<<<<<<< HEAD
 	void Destroy(){
 		if(_currentWaypoint >= (waypoints.Count - 1))
 			Destroy(gameObject);
-=======
-	void Destroy()
-	{
-		Destroy(gameObject);
-		//Camera.main.gameObject.GetComponent<GoldScript>().playerOwnedCoin += 2;
->>>>>>> origin/master
 	}
 	
 }
